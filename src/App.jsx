f12180c6473b1437d3a5f68e762d2e3e14bf28e8
FabioMarras <fabio.marras@yahoo.com>
2023-09-25 10:22:20 +0200

    first div

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Aside from "./components/Aside";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Aside />
+    </div>
+  );
 }
 
 export default App;
