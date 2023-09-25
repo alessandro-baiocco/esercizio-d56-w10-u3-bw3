91346c795f1ffc92731317bb36838bc3772ff1b0
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-09-25 10:03:50 +0200

    card

--- src/components/profilemain.jsx
+++ src/components/profilemain.jsx
@@ -1,2 +1,21 @@
-const ProfileMain = () => {};
+import { Button, Card, Container } from "react-bootstrap";
+
+const ProfileMain = () => {
+  return (
+    <>
+      <Container>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Container>
+    </>
+  );
+};
 export default ProfileMain;
