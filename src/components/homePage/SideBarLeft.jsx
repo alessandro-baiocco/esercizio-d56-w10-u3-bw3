10825d4be0485472f5176f00ba61ffe12a2d090e
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-09-27 15:58:32 +0200

    changed background

--- src/components/homePage/SideBarLeft.jsx
+++ src/components/homePage/SideBarLeft.jsx
@@ -9,7 +9,7 @@ const SideBarLeft = () => {
         <div>
           <Image
             src={
-              "https://images.unsplash.com/photo-1683009427619-a1a11b799e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              "https://images.unsplash.com/photo-1439405326854-014607f694d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
             }
             width={"200px"}
           />
--- src/components/profileMain/ProfileHero.jsx
+++ src/components/profileMain/ProfileHero.jsx
@@ -62,7 +62,7 @@ const ProfileHero = (props) => {
           <Card>
             <Card.Img
               variant="top"
-              src="https://images.unsplash.com/photo-1683009427619-a1a11b799e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              src="https://images.unsplash.com/photo-1439405326854-014607f694d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
               height={"200px"}
               style={{ position: "relative" }}
             />
