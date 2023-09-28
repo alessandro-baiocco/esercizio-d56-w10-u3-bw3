6cecf99caca6721cc9a2fccb5c76dd88ad297ecd
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-09-28 09:54:25 +0200

    start reducer

--- src/components/jobPage/GetJobsPage.jsx
+++ src/components/jobPage/GetJobsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Col, Container, ListGroup, Row } from "react-bootstrap";
 import { HandThumbsDown, HandThumbsUp } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,6 +9,7 @@ import SecondaryFooter from "../footer/SecondaryFooter";
 const GetJobsPage = () => {
   const jobs = useSelector((state) => state.getJobs.content);
   const dispatch = useDispatch();
+  const [search, setSearch] = useState();
 
   const navigate = useNavigate();
 
@@ -28,35 +29,37 @@ const GetJobsPage = () => {
               </p>
             </ListGroup.Item>
             {jobs &&
-              jobs.map((job) => (
-                <ListGroup.Item className="p-4">
-                  <div className="d-flex">
-                    <div>
-                      <img src="https://placehold.co/50x50" alt="" />
+              jobs
+                .filter((job) => job.title.toLowerCase().includes(""))
+                .map((job) => (
+                  <ListGroup.Item className="p-4">
+                    <div className="d-flex">
+                      <div>
+                        <img src="https://placehold.co/50x50" alt="" />
+                      </div>
+                      <div className="ms-2">
+                        <h6 className="mb-1" style={{ fontSize: "18px" }}>
+                          <Link to={job.url} style={{ textDecoration: "none" }} className="text-primary">
+                            {job.title}
+                          </Link>
+                        </h6>
+                        <p className="mb-0" style={{ fontSize: "14px" }}>
+                          {job.company_name}
+                        </p>
+                        <p className="mb-0 opacity-75" style={{ fontSize: "14px" }}>
+                          {job.candidate_required_location}
+                        </p>
+                        <p className="mb-0 opacity-75" style={{ fontSize: "14px" }}>
+                          Pubblicato il: {job.publication_date.slice(0, 10)}
+                        </p>
+                      </div>
+                      <div className="d-flex ms-auto gap-3">
+                        <HandThumbsUp className="fs-5" />
+                        <HandThumbsDown className="fs-5" />
+                      </div>
                     </div>
-                    <div className="ms-2">
-                      <h6 className="mb-1" style={{ fontSize: "18px" }}>
-                        <Link to={job.url} style={{ textDecoration: "none" }} className="text-primary">
-                          {job.title}
-                        </Link>
-                      </h6>
-                      <p className="mb-0" style={{ fontSize: "14px" }}>
-                        {job.company_name}
-                      </p>
-                      <p className="mb-0 opacity-75" style={{ fontSize: "14px" }}>
-                        {job.candidate_required_location}
-                      </p>
-                      <p className="mb-0 opacity-75" style={{ fontSize: "14px" }}>
-                        Pubblicato il: {job.publication_date.slice(0, 10)}
-                      </p>
-                    </div>
-                    <div className="d-flex ms-auto gap-3">
-                      <HandThumbsUp className="fs-5" />
-                      <HandThumbsDown className="fs-5" />
-                    </div>
-                  </div>
-                </ListGroup.Item>
-              ))}
+                  </ListGroup.Item>
+                ))}
           </ListGroup>
         </Col>
         <Col xs="3">
