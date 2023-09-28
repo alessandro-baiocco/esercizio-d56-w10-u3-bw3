c334880eada51bdba59d01112d7ed8042a4590cf
Andrea De Masi <andreademasi1999@gmail.com>
2023-09-28 12:21:47 +0200

    css updating

--- src/components/myNav.jsx
+++ src/components/myNav.jsx
@@ -99,7 +99,7 @@ const MyNav = () => {
         </div>
         {!isActive && (
           <Nav className="me-auto d-none d-sm-flex">
-            <Nav.Link href="/" className="mx-2 widthIconNavBar">
+            <Nav.Link onClick={() => navigate("/")} className="mx-2 widthIconNavBar">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
@@ -125,7 +125,7 @@ const MyNav = () => {
               </svg>
               <p className={isActive ? "d-none" : "mb-0 d-none d-md-block"}>Rete </p>
             </Nav.Link>
-            <Nav.Link to="#link" className="mx-2 widthIconNavBar">
+            <Nav.Link onClick={() => navigate("/jobs")} className="mx-2 widthIconNavBar">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
